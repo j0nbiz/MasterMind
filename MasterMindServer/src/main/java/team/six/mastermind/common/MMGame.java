e2080e03e5d07455b321594323c9f8fa45445027
Jonathan Bizier <j0nbiz@outlook.com>
2016-10-20 13:42:07 -0400

    started working on interpreting a guess

--- MasterMindServer/src/main/java/team/six/mastermind/common/MMGame.java
+++ MasterMindServer/src/main/java/team/six/mastermind/common/MMGame.java
@@ -35,10 +35,37 @@ public class MMGame {
         return this.round;
     }
 
-    public boolean interpret(MMPacket guess) {
+    public MMPacket interpret(MMPacket guess) throws IOException {
         // Increment round count on every guess
         round++;
 
-        return answer.equals(guess);
+        // Check for good answer
+        if(guess.equals(answer)){
+            return new MMPacket((byte) 1, (byte) 1, (byte) 1, (byte) 1);
+        }
+        
+        // byte array that will be used to populate returned MMPacket hint
+        // 0 being no peg, 1 being a white peg, 2 being a black peg
+        byte[] hints = new byte[]{0,  0,  0,  0};
+        
+        for(int i = 0; i < guess.getBytes().length; i++){
+            if(guess.getBytes()[i] == answer.getBytes()[i]){
+                hints[i] = 1;
+            }else{
+                for(int j = 0; i < answer.getBytes().length; i++){
+                    if(guess.getBytes()[i] == answer.getBytes()[j]){
+                        hints[i] = 2;
+                    }
+                }
+            }
+        }
+        
+        // Assert color
+        if(guess.getBytes())
+        
+        // Assert position
+        
+        
+        return new MMPacket(hints[0], hints[1], hints[2], hints[3]);
     }
 }
